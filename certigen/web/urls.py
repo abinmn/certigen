9aec000a338ea6f837e637faf37413e3ae16cadd
Abin Mn <abinmn619@gmail.com>
2020-12-25 20:19:06 +0530

    Add Login

--- certigen/web/urls.py
+++ certigen/web/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import accounts
 
 urlpatterns = [
-    
+    path('login', accounts.SignInView.as_view(), name='login'),
+    path('dashboard', accounts.SignInView.as_view(), name='dashboard'),
 ]
