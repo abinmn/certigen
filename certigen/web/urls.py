2646efdc62cdeeb276d13c439177418030f27033
Abin Mn <abinmn619@gmail.com>
2021-01-14 16:52:38 +0530

    Rename events module to event

--- certigen/web/urls.py
+++ certigen/web/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import accounts
 from .views import dashboard
-from .views import events
+from .views import event
 
 urlpatterns = [
     path('login', accounts.SignInView.as_view(), name='login'),
     path('', dashboard.DashboardView.as_view(), name='dashboard'),
-    path('events', events.EventListView.as_view(), name='events'),
-    path('events/create', events.EventCreateView.as_view(), name='events-create'),
+    path('events', event.EventListView.as_view(), name='events'),
+    path('events/create', event.EventCreateView.as_view(), name='events-create'),
     path('emails', dashboard.DashboardView.as_view(), name='emails'),
 ]
